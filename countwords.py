c6203676b74795df6fe7cdfe35b6474699d20901
deeps120 <62687789+deeps120@users.noreply.github.com>
2020-11-15 12:00:25 +0530

    Add files via upload

---
+++ countwords.py
@@ -0,0 +1,9 @@
+introstring=input("enter your introduction")
+charactercount=0
+wordcount=1
+for i in introstring:
+    charactercount=charactercount+1
+    if(i==' '):
+        wordcount=wordcount+1
+print (wordcount)    
+print (charactercount)    
